b03d0850cd2de7262653d26db599d04f6e2f5fd2
Rennison <mathengerennison015@gmail.com>
2024-12-09 08:40:02 +0300

    Changes made

--- injector/urls.py
+++ injector/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('service/', views.service, name='service'),
+    path('update_item/', views.updateItem, name='update_item'),
+    path('process_order/', views.processOrder, name="process_order"),
+
 ]
